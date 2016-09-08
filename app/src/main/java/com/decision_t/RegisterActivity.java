3e4f0959890424d9a4d7a01e306efedbe7c4abbd
張明仁 <a10423041@yuntech.org.tw>
2016-09-08 18:53:15 +0800

    創建RegisterActivity 1.修正LoginActivity的繼承，改為繼承MainActivity
    Signed-off-by: 張明仁 <a10423041@yuntech.org.tw>

---
+++ app/src/main/java/com/decision_t/RegisterActivity.java
@@ -0,0 +1,12 @@
+package com.decision_t;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
