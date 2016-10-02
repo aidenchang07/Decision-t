e7054e04938db75dd2d91b5e39c5c8d9df7c7bdd
robinaiden <a10423041@yuntech.org.tw>
2016-10-02 23:47:38 +0800

    新增 r_table 的右上角 info icon -新增點擊右上角的 info icon，可開啟右側 NavigationView -新增一些 icon 圖示
    Signed-off-by: robinaiden <a10423041@yuntech.org.tw>

--- app/src/main/java/com/decision_t/R_Table_Activity.java
+++ app/src/main/java/com/decision_t/R_Table_Activity.java
@@ -1,13 +1,67 @@
 package com.decision_t;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class R_Table_Activity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.r_table_activity_main);
+
+        navigationView = (NavigationView) findViewById(R.id.r_table_nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                }
+                return false;
+            }
+        });
+
+        toolbar = (Toolbar) findViewById(R.id.r_table_toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.r_table_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    //創建右上角的 info
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.r_table_toolbar_menu, menu);
+        return true;
+    }
+
+    //info 被點到會有所反應
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_info:
+                drawer.openDrawer(Gravity.RIGHT);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
