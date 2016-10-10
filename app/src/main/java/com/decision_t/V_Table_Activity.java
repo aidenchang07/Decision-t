e97a9db8f96e97293d70c0fc9c0012db6d9518bb
robinaiden <a10423041@yuntech.org.tw>
2016-10-10 14:52:44 +0800

    新增 v_table 主頁面
    Signed-off-by: robinaiden <a10423041@yuntech.org.tw>

---
+++ app/src/main/java/com/decision_t/V_Table_Activity.java
@@ -0,0 +1,13 @@
+package com.decision_t;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class V_Table_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.v_table_activity_main);
+    }
+}
