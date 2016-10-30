9fe02fb028faacbfd49fe48141cd1e3b7ba0e1c1
robinaiden <a10423041@yuntech.org.tw>
2016-10-30 22:56:48 +0800

    新增 t_table_tabs(尚未完全建立好)
    Signed-off-by: robinaiden <a10423041@yuntech.org.tw>

---
+++ app/src/main/java/com/decision_t/T_Table_Tab_Activity.java
@@ -0,0 +1,29 @@
+package com.decision_t;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class T_Table_Tab_Activity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.t_table_tab);
+
+        /** 初始化各元件 */
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        /** 取代舊版ActionBar */
+        setSupportActionBar(toolbar);
+
+    }
+}
