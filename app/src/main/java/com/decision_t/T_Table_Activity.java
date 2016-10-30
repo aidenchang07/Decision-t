e24804da7cb7df18ae04ca1c22be1f331a0dd62a
robinaiden <a10423041@yuntech.org.tw>
2016-10-30 14:19:56 +0800

    新增 t_table 頁面
    Signed-off-by: robinaiden <a10423041@yuntech.org.tw>

---
+++ app/src/main/java/com/decision_t/T_Table_Activity.java
@@ -0,0 +1,13 @@
+package com.decision_t;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class T_Table_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.t_table_activity_main);
+    }
+}
