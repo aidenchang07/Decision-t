ae104323f4638489f03c1510b8de03b588b94dd3
張明仁 <a10423041@yuntech.org.tw>
2016-11-05 22:58:49 +0800

    Fixed: tab 架構初步建好
    Signed-off-by: 張明仁 <a10423041@yuntech.org.tw>

--- app/src/main/java/com/decision_t/T_Table_Tab_Activity.java
+++ app/src/main/java/com/decision_t/T_Table_Tab_Activity.java
@@ -5,12 +5,14 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class T_Table_Tab_Activity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +23,33 @@ public class T_Table_Tab_Activity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        /** 這裡的"支持"與"不支持"字串，是Tab上的String */
+        viewPagerAdapter.addFragments(new SupportFragment(), "支持");
+        viewPagerAdapter.addFragments(new NotSupportFragment(), "不支持");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
         /** 取代舊版ActionBar */
         setSupportActionBar(toolbar);
+        /** 左上角出現返回鍵 */
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case android.R.id.home:
+                /** 對用戶按home icon的處理，本例只需關閉activity，就可返回上一activity，即主activity。 */
+                finish();
+                return true;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
