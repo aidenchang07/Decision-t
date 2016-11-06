4c7d4cced95289cb17763b603fe7a9b831646837
張明仁 <a10423041@yuntech.org.tw>
2016-11-06 22:14:03 +0800

    Add: 在 t_table_tab 上，點擊右上角的 info 可彈出右側滑欄
    Signed-off-by: 張明仁 <a10423041@yuntech.org.tw>

--- app/src/main/java/com/decision_t/T_Table_Tab_Activity.java
+++ app/src/main/java/com/decision_t/T_Table_Tab_Activity.java
@@ -1,10 +1,15 @@
 package com.decision_t;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.decision_t.t_table_tab.NotSupportFragment;
@@ -17,6 +22,8 @@ public class T_Table_Tab_Activity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private ViewPagerAdapter viewPagerAdapter;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,7 @@ public class T_Table_Tab_Activity extends AppCompatActivity {
         /** 初始化各元件 */
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        drawer = (DrawerLayout) findViewById(R.id.t_table_tab_drawer_layout);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         /** 這裡的"支持"與"不支持"字串，是Tab上的String */
@@ -34,6 +42,17 @@ public class T_Table_Tab_Activity extends AppCompatActivity {
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
 
+        //右側欄menu初始化
+        navigationView = (NavigationView) findViewById(R.id.t_table_tab_nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //按完之後關起來
+                drawer.closeDrawer(GravityCompat.END);
+                return true;
+            }
+        });
+
         /** 取代舊版ActionBar */
         setSupportActionBar(toolbar);
         /** 左上角出現返回鍵 */
@@ -41,18 +60,27 @@ public class T_Table_Tab_Activity extends AppCompatActivity {
 
     }
 
+    //創建右上角的 info
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    //info 被點到會有所反應
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch(item.getItemId()){
             case android.R.id.home:
                 /** 對用戶按home icon的處理，本例只需關閉activity，就可返回上一activity，即主activity。 */
                 finish();
                 return true;
+            case R.id.action_info:
+                drawer.openDrawer(GravityCompat.END);
+                return true;
             default:
                 break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
