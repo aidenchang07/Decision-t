32e3ec6745be7cf3944c16aa26cd86fe83f68665
張明仁 <a10423041@yuntech.org.tw>
2016-11-06 10:57:33 +0800

    Add: 加入 listview 至 fragment 下 Fix: listview 不被 toolbar overlap
    Signed-off-by: 張明仁 <a10423041@yuntech.org.tw>

--- app/src/main/java/com/decision_t/t_table_tab/NotSupportFragment.java
+++ app/src/main/java/com/decision_t/t_table_tab/NotSupportFragment.java
@@ -22,7 +22,6 @@ public class NotSupportFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_not_support, container, false);
     }
 
--- app/src/main/java/com/decision_t/t_table_tab/SupportFragment.java
+++ app/src/main/java/com/decision_t/t_table_tab/SupportFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.decision_t.R;
 
@@ -22,7 +24,6 @@ public class SupportFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_support, container, false);
     }
 
