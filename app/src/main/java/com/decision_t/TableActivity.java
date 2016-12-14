a743db09162a888af5281c0e2aa46816400ee533
robinaiden <a10423041@yuntech.org.tw>
2016-12-14 13:21:35 +0800

    Fix: 修正了 floatingbutton 的顏色為 logo藍

--- app/src/main/java/com/decision_t/TableActivity.java
+++ app/src/main/java/com/decision_t/TableActivity.java
@@ -5,7 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
+import com.github.clans.fab.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
