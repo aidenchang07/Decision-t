cfc361b9c0e7f4677b14151fb0e0462d86d09232
Aiden Chang <asdf62210@gmail.com>
2024-07-03 23:50:38 +0800

    Refactor: Use TableConfig.kt
    Solution:
    使用kotlin寫法

---
+++ app/src/main/java/com/decision_t/config/TableConfig.kt
@@ -0,0 +1,10 @@
+package com.decision_t.config
+
+/**
+ * Created by AidenChang 2024/07/03
+ */
+object TableConfig {
+    const val TABLE_R = "R"
+    const val TABLE_V = "V"
+    const val TABLE_T = "T"
+}
